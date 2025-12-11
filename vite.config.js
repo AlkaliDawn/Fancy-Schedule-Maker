f77ba027198e312895ab55b8f060ba27d2fbab94
AlkaliDawn <evanbpear@gmail.com>
2025-12-10 21:45:54 -0600

    Refactor and remove special hash treatment (I learned how the public folder works)

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    // Keep logo.png without hash
-                    if (assetInfo.name === 'UofM_Logo.png') {
-                        return 'assets/UofM_Logo.png'
-                    }
-                    // Everything else gets hashed normally
-                    return 'assets/[name]-[hash][extname]'
-                }
-            }
-        }
-    }
 })
